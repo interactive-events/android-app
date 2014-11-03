ebb5a1cc257aba09383619fde32439251ba77212
Oskar Klang <raxo@pub93-130.pub.ltu.se>
2014-11-03 12:51:01 +0100

    Event activity initial commit, logins is main activity!

---
+++ app/src/main/java/com/wordpress/interactiveevents/interactive_events/EventListActivity.java
@@ -0,0 +1,17 @@
+package com.wordpress.interactiveevents.interactive_events;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by raxo on 03/11/14.
+ */
+public class EventListActivity extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_list);
+
+
+    }
+}
