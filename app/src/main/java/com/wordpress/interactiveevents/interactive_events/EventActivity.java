11bd78fa488d926861ccb85062b5cfa1e8b05219
Sofie Nilsson <sofnii-9@student.ltu.se>
2014-11-12 16:37:54 +0100

    Added Jackson and a working sample request, requesting some arbitrary stuff

--- app/src/main/java/com/wordpress/interactiveevents/interactive_events/EventActivity.java
+++ app/src/main/java/com/wordpress/interactiveevents/interactive_events/EventActivity.java
@@ -6,11 +6,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.util.Log;
+
+import com.octo.android.robospice.JacksonSpringAndroidSpiceService;
+import com.octo.android.robospice.SpiceManager;
+import com.octo.android.robospice.persistence.DurationInMillis;
+import com.octo.android.robospice.persistence.exception.SpiceException;
+import com.octo.android.robospice.request.listener.RequestListener;
+
 
 public class EventActivity extends Activity{
     private Button homeBtn;
     TextView event_description;
     TextView event_title;
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,5 +49,65 @@ public class EventActivity extends Activity{
                 startActivity(homeScreen);
             }
         });
+
+        final Button reqButton = (Button) findViewById(R.id.button3);
+        reqButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                performRequest("steffengy");
+            }
+        });
+    }
+
+    //------------------------------------------------------------------------
+    //this block can be pushed up into a common base class for all activities
+    //------------------------------------------------------------------------
+
+    protected SpiceManager spiceManager = new SpiceManager(JacksonSpringAndroidSpiceService.class);
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        spiceManager.start(this);
+    }
+
+    @Override
+    protected void onStop() {
+        spiceManager.shouldStop();
+        super.onStop();
+    }
+
+    //------------------------------------------------------------------------
+    //---------end of block that can fit in a common base class for all activities
+    //------------------------------------------------------------------------
+
+    private void performRequest(String user) {
+        EventActivity.this.setProgressBarIndeterminateVisibility(true);
+
+        FollowersRequest request = new FollowersRequest(user);
+        String lastRequestCacheKey = request.createCacheKey();
+
+        // use DurationInMillis.ALWAYS_EXPIRED and you will never used cached values
+        spiceManager.execute(request, lastRequestCacheKey, DurationInMillis.ONE_MINUTE, new ListFollowersRequestListener());
+    }
+
+    //inner class of your spiced Activity
+    private class ListFollowersRequestListener implements RequestListener<FollowerList> {
+
+        @Override
+        public void onRequestFailure(SpiceException e) {
+            //update your UI
+            Log.e("robospice", "Fail on request", e);
+        }
+
+        @Override
+        public void onRequestSuccess(FollowerList listFollowers) {
+            //update your UI
+            Log.d("robospice", listFollowers.toString());
+            for (Follower f : listFollowers) {
+                System.out.println(f.getLogin());
+                Log.d("robospice", f.getLogin());
+            }
+        }
     }
 }
