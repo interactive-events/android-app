4731e3a8fe1096309d85cfdaa56e8567cba1b002
webbpiraten <webbpiraten@gmail.com>
2014-11-13 10:33:34 +0100

    Major and Minor update

--- app/src/main/java/com/wordpress/interactiveevents/interactive_events/BleActivity.java
+++ app/src/main/java/com/wordpress/interactiveevents/interactive_events/BleActivity.java
@@ -2,24 +2,34 @@ package com.wordpress.interactiveevents.interactive_events;
 
 import android.app.Activity;
 import android.app.Application;
+import android.app.IntentService;
 import android.content.Intent;
+import android.os.RemoteException;
 import android.util.Log;
 import android.widget.Toast;
 
+import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
 import org.altbeacon.beacon.Identifier;
 import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.service.RangingData;
 import org.altbeacon.beacon.startup.BootstrapNotifier;
 import org.altbeacon.beacon.startup.RegionBootstrap;
+import org.altbeacon.beacon.RangeNotifier;
+
+import java.lang.reflect.Array;
+import java.util.Collection;
 
 /**
  * Created by Webbpiraten on 2014-11-10.
  */
-public class BleActivity extends Application implements BootstrapNotifier{
+public class BleActivity extends Application implements BootstrapNotifier, RangeNotifier{
     private static final String TAG = ".BleActivity";
     private RegionBootstrap regionBootstrap;
+    private String[] BeaconMinMaj;
+    private BeaconManager bm;
 
     public void onCreate() {
         super.onCreate();
@@ -30,29 +40,50 @@ public class BleActivity extends Application implements BootstrapNotifier{
 
         BeaconManager.getInstanceForApplication(this).getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:0-3=4c000215,i:4-19,i:20-21,i:22-23,p:24-24"));
 
+        // Identifier.fromInt(1)
         Region region = new Region("com.wordpress.interactiveevents.interactive_events",Identifier.parse("B9407F30-F5F8-466E-AFF9-25556B57fE6D"), null, null);
         regionBootstrap = new RegionBootstrap(this, region);
-
-        BeaconManager bm = BeaconManager.getInstanceForApplication(this);
+        bm = BeaconManager.getInstanceForApplication(this);
         bm.setBackgroundScanPeriod(2000); // 2 seconds
         bm.setBackgroundBetweenScanPeriod(15000); // 15 seconds
+
+        bm.setRangeNotifier(this);
+
 
     }
 
     @Override
-    public void didEnterRegion(Region arg0) {
+    public void didEnterRegion(Region region) {
         Log.d(TAG, "###################################### Did enter region ######################################");
         Intent intent = new Intent(getApplicationContext(), PositiveActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         // Important:  make sure to add android:launchMode="singleInstance" in the manifest
         // to keep multiple copies of this activity from getting created if the user has
         // already manually launched the app.
+        //Intent intser = new Intent(getApplicationContext(), BeaconDataService.class);
+        try {
+            bm.startRangingBeaconsInRegion(region);
+        } catch (RemoteException e){
+            e.printStackTrace();
+        }
+
         startActivity(intent);
+
+        //intser.putExtra("test", BeaconMinMaj);
+        //startService(intser);
+
     }
     @Override
     public void didDetermineStateForRegion(int i, Region region) {
     }
     @Override
     public void didExitRegion(Region region) {
+    }
+
+    @Override
+    public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+        for (Beacon beacon: beacons){
+            Log.i(TAG, "Beacon detected with id1: "+beacon.getId1()+" id2:"+beacon.getId2()+" id3: "+beacon.getId3());
+        }
     }
 }
