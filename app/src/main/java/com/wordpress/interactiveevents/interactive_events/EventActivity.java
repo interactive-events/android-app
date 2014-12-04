50bbf3e34a7949aabf32c66e8af9ff237a183fe2
sansav <santtu.savukoski@outlook.com>
2014-12-04 10:21:21 +0100

    ui fixing
    randomnezz aoeoeoe

--- app/src/main/java/com/wordpress/interactiveevents/interactive_events/EventActivity.java
+++ app/src/main/java/com/wordpress/interactiveevents/interactive_events/EventActivity.java
@@ -3,12 +3,15 @@ package com.wordpress.interactiveevents.interactive_events;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.octo.android.robospice.JacksonSpringAndroidSpiceService;
@@ -18,12 +21,21 @@ import com.octo.android.robospice.persistence.exception.SpiceException;
 import com.octo.android.robospice.request.listener.RequestListener;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
-public class EventActivity extends Activity{
-    private Button homeBtn;
+public class EventActivity extends Activity {
+    private ImageButton homeBtn;
     TextView event_description;
     TextView event_title;
+    SeekBar seek_bar;
+    long currTi = 0;
+    long progressTime = 0;
+    long startTi = 0;
+    long endTi = 0;
+    Timer timer = null;
 
     ArrayList<String> moduleStringArr = new ArrayList<String>();
     ArrayAdapter<String> adapter;
@@ -33,20 +45,30 @@ public class EventActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_view);
-
+        currTi = System.currentTimeMillis();
         event_description = (TextView) findViewById(R.id.event_description);
+        event_description.setMovementMethod(new ScrollingMovementMethod());
         event_title = (TextView) findViewById(R.id.event_title);
+        seek_bar = (SeekBar) findViewById(R.id.seekBar);
 
         Bundle extras = getIntent().getExtras();
+
         if (extras != null) {
             eventId = extras.getString("eventId");
             String title = extras.getString("event_title");
             String description = extras.getString("event_desc");
+            startTi = extras.getLong("start_time");
+            endTi = extras.getLong("stop_time");
             event_description.setText(description);
             event_title.setText(title);
+            seek_bar.setEnabled(false);
+
+
+            updateProgress(startTi, endTi);
         }
 
-        homeBtn = (Button) findViewById(R.id.button2);
+
+        homeBtn = (ImageButton) findViewById(R.id.button2);
         homeBtn.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -72,19 +94,17 @@ public class EventActivity extends Activity{
         performEventModulesRequest(eventId);
 
         // see syntax for overriding methods without extending a class explicitly
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, final int position,
-                                    long id)
-            {
+                                    long id) {
                 // parent = ListView lv, view = view for the item that was clicked on
                 // position = position for view in the adapter
                 // id = row id of the item that was clicked on
-                Log.d("modulelist", "position="+position+" id="+id);
+                Log.d("modulelist", "position=" + position + " id=" + id);
 
                 // Assuming string, get value of clicked item
-                String value = (String)parent.getItemAtPosition(position);
+                String value = (String) parent.getItemAtPosition(position);
                 Log.d("modulelist", value);
 
                 //Starting a new Intent
@@ -100,7 +120,42 @@ public class EventActivity extends Activity{
         });
     }
 
+    class SayHello extends TimerTask {
+        public void run() {
+            updateProgress(startTi, endTi);
+            Log.i("AIDSBULLAR","jag är här varje 5s");
+        }
+    }
+
+    // And From your main() method or any other method
+
+
+
+    public void updateProgress(long startTi, long endTi){
+
+        currTi = System.currentTimeMillis();
+        long timeFragment1 = currTi - startTi;
+        long timeFragment2 = endTi - startTi;
+        progressTime = (long) ((float) timeFragment1 / timeFragment2 * 100);
+
+        Log.i("ASDASD AWTF IS HAPPENINZ", " currTi=" + currTi +" startTi=" + startTi + " endTi=" + endTi + " progressTime=" + progressTime);
+
+        if (progressTime > 0 && progressTime <= 100) {
+            Log.i("ASDASD AWTF IS HAPPENINZ", "In da if prog > 0 && prog <= 100| " + progressTime);
+            seek_bar.setProgress((int) progressTime);
+        } else if (progressTime < 0){
+            Log.i("ASDASD AWTF IS HAPPENINZ", "In da elze prog < 0| " + progressTime);
+            seek_bar.setProgress(0);
+        } else if (progressTime > 100){
+            Log.i("ASDASD AWTF IS HAPPENINZ", "In da elze prog > 100| " + progressTime);
+            seek_bar.setProgress(100);
+        }
+    }
+
+
     protected void onResume() {
+        timer = new Timer();
+        timer.schedule(new SayHello(), 0, 5000);
         super.onResume();
         performEventModulesRequest(eventId);
     }
@@ -121,6 +176,8 @@ public class EventActivity extends Activity{
     protected void onStop() {
         spiceManager.shouldStop();
         super.onStop();
+        finish();
+        timer.cancel();
     }
 
     //------------------------------------------------------------------------
